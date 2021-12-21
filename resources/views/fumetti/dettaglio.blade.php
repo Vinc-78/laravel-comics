cf39a6962b503df04cafb930fbbe5710b66dfce6
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-12-21 22:32:32 +0100

    concluso con bonus

--- resources/views/fumetti/dettaglio.blade.php
+++ resources/views/fumetti/dettaglio.blade.php
@@ -17,6 +17,9 @@
 
     <div class="vetrina">
         <img class="w-75" src="{{$fumetto_singolo['thumb']}}" alt=""> 
+            <div class="go-galleria">
+                <a href="{{route('home.comics')}}" > Vai alla Galleria </a>
+            </div>
     </div>
 
     <div class="row pt-4">
@@ -39,7 +42,7 @@
 
                 
             </div>
-            <div class="descrizione">
+            <div class="descrizione py-1">
                 <p>{{$fumetto_singolo['description']}}</p>
             </div>
 
@@ -50,6 +53,100 @@
             <img class="w-75" src="{{ asset('img/adv.jpg') }}" alt="">
             
         </div> 
+
+    </div>
+
+
+</div>
+
+<div class="container-fluid sezione-dettagli mt-3">
+
+    <div class="container">
+        <div class="row g-4">
+            <div class="col-6">
+                <h4 class="text-secondary border-bottom py-4 mb-4">Talent</h4>
+
+                <div class="art border-bottom d-flex mb-4">
+
+                    <div class="title">
+                        <span>Arta By:</span>
+                    </div>
+
+                    <div class="artists">
+
+                        @foreach ($fumetto_singolo['artists'] as $item)
+                            <span>U.S: Price: {{$item}} </span>
+                        @endforeach
+                        
+                    </div>
+
+                </div>
+
+                <div class="art border-bottom d-flex mb-4">
+
+                    <div class="title">
+                        <span>Writers By:</span>
+                    </div>
+
+                    <div class="artists">
+
+                        @foreach ($fumetto_singolo['writers'] as $item)
+                            <span>U.S: Price: {{$item}} </span>
+                        @endforeach
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-6">
+                <h4 class="text-secondary border-bottom py-4 mb-2">Species</h4>
+
+                <div class="art border-bottom d-flex mb-2">
+
+                    <div class="title">
+                        <span>Series</span>
+                    </div>
+
+                    <div class="dettagli">
+
+                        <p>{{$fumetto_singolo['series']}}</p>
+                        
+                    </div>
+
+                </div>
+
+                <div class="art border-bottom d-flex mb-2">
+
+                    <div class="title">
+                        <span>US Price:</span>
+                    </div>
+
+                    <div class="dettagli">
+
+                        <p>{{$fumetto_singolo['price']}}</p>
+                        
+                    </div>
+
+                </div>
+                <div class="art border-bottom d-flex mb-2">
+
+                    <div class="title">
+                        <span>On Sale Date:</span>
+                    </div>
+
+                    <div class="dettagli">
+
+                        <p>{{$fumetto_singolo['sale_date']}}</p>
+                        
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </div>
 
     </div>
 
